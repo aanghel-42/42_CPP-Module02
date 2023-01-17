4609c79a440b2215bfa3f7e876438b7c98a0bb20
iri-angh <irina.anghel42@gmail.com>
2023-01-18 00:30:33 +0100

    subject and ex00

---
+++ ex00/src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanghel <aanghel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 00:25:55 by aanghel           #+#    #+#             */
+/*   Updated: 2023/01/18 00:27:33 by aanghel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Fixed.hpp"
+
+int main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
