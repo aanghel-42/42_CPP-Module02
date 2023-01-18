91449b41827ae0046f3108dfe707af65c4d2a081
iri-angh <irina.anghel42@gmail.com>
2023-01-19 00:41:51 +0100

    ex00

--- ex00/inc/Fixed.hpp
+++ ex00/inc/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: aanghel <aanghel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 00:25:51 by aanghel           #+#    #+#             */
-/*   Updated: 2023/01/18 00:29:18 by aanghel          ###   ########.fr       */
+/*   Updated: 2023/01/19 00:39:11 by aanghel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,17 @@
 class Fixed
 {
 	private:
-
-
+		int fixed_point;
+		static const int fractional_bits = 8;
 
 	public:
+		Fixed();
+		Fixed(const Fixed& copy);
+		Fixed& operator = (const Fixed& copy);
+		~Fixed();
+
+		int getRawBits(void)const;
+		void setRawBits(int const raw);
 	
 };
 
--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,44 @@
 /*   By: aanghel <aanghel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 00:25:53 by aanghel           #+#    #+#             */
-/*   Updated: 2023/01/18 00:25:54 by aanghel          ###   ########.fr       */
+/*   Updated: 2023/01/19 00:38:59 by aanghel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor callen\n";
+	this->fixed_point = 0;
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+	std::cout << "Copy constructor called\n";
+	this->fixed_point = copy.fixed_point;
+}
+
+Fixed& Fixed::operator=(const Fixed& copy)
+{
+	std::cout << "Copy assignemnet operator called\n";
+	if (this != &copy)
+		this->fixed_point = copy.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Decostructor called\n";
+} 
+
+int Fixed::getRawBits(void)const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixed_point);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->fixed_point = raw;
+}
